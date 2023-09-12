16859c72a8eb996bc574547de56c6d7d7d8169ed
Matt Gabrenya <matt@buildyourweb.app>
2023-09-12 00:22:45 -0700

    feat: inject renderer.ts into index.html file on vite build

--- vite.renderer.config.ts
+++ vite.renderer.config.ts
@@ -1,7 +1,19 @@
 import path from 'path';
 import { defineConfig } from 'vite';
+import { createHtmlPlugin } from 'vite-plugin-html'
 
 // https://vitejs.dev/config
 export default defineConfig({
   root: path.resolve(__dirname, "src"),
+  plugins: [
+    {
+      name: "html-inject-renderer-into-head",
+      enforce: "post",
+      transformIndexHtml(html: string) {
+          const regex = /<\/head>(.*?)/gi;
+          const replacement = '<script type="module" src="/renderer.ts"></script>\n</head>$1';
+          return html.replace(regex, replacement);
+      },
+    },
+  ]
 });
