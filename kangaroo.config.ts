8e0d64d6259a598bb0f8bb644560a56315177c34
Matthias <tuttiacc@hotmail.com>
2024-12-12 11:35:42 +0100

    bump to holochain 0.4.0-rc.2

--- kangaroo.config.ts
+++ kangaroo.config.ts
@@ -9,29 +9,29 @@ export default defineConfig({
   fallbackToIndexHtml: true,
   bins: {
     holochain: {
-      version: "0.4.0-rc.1",
+      version: "0.4.0-rc.2",
       sha256: {
         "x86_64-unknown-linux-gnu":
-          "6e111ff588628c8da0b64340a0e2fa9291146c5033b21102ce115fbecf8aa36c",
+          "7110f9b0c9c2b4bbc974543df1b633299c7fa8d3d16f3cde4dc894ac64c0670f",
         "x86_64-pc-windows-msvc.exe":
-          "4268aac469304cd1b6bb6a90631a3a018e6f08a236088e7a7045a9969ba4458f",
+          "959fca4b575c6791196fcd1fb9c6c7a92359191dbd60e95b7800c6e79bb21687",
         "x86_64-apple-darwin":
-          "b377612a817310dc4f747662003e68b96b9058d22464d28db8b807cbe7d6704d",
+          "a62d1b6413f2791ec4df87338d794a7ba29a0890f1a2ceb1ed9eeddb0efc94a7",
         "aarch64-apple-darwin":
-          "23b60ae414b1525a41aabcd213b29922cc4f581f3d748750393357847b10f3fb",
+          "f25bee4cb2615b1970a8ef89c82dcfb867682a3502962343d4842571aafb9bc3",
       },
     },
     lair: {
-      version: "0.5.2",
+      version: "0.5.3",
       sha256: {
         "x86_64-unknown-linux-gnu":
-          "5fa1b2ece8896208c313c01b531d99d861e156de8f2d2ddc2709a82bc2533550",
+          "96a28b9b37c73ef46d8b5c56b9d799d558fd2fe77b41c577e2bcb37685a46396",
         "x86_64-pc-windows-msvc.exe":
-          "42a4a6eaf3cd1cb52bdc6512b392e874467bc6d7f5763b89a303767a9d979ad9",
+          "68b6453a19921072aac04dae52a4e94e725e7482005d2f54f907aec680e078de",
         "x86_64-apple-darwin":
-          "22cadfb73435ce0c97e6581123b4fc3fe9ff0a5ef5b04eed3b4895a84f5cbb79",
+          "a53bfb8e501431870b99243cbac24f6103d67f8be094930f174829bb249f34c4",
         "aarch64-apple-darwin":
-          "246bb1090e9e875babe056df413f75ad8dd1ba4971f293dd5e94ea05ea364aa0",
+          "6b15d977408847ac977c2e060c7aab84a69e6e90c79390098dd40a6b75256e50",
       },
     },
   },
