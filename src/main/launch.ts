684b1445ee73e55859b68726cb730e26123fcd10
Matthias <tuttiacc@hotmail.com>
2025-04-10 15:25:03 +0200

    pass along rust logs

--- src/main/launch.ts
+++ src/main/launch.ts
@@ -85,7 +85,9 @@ export async function launch(
     lairUrl,
     runOptions.bootstrapUrl ? runOptions.bootstrapUrl.toString() : DEFAULT_BOOTSTRAP_SERVER,
     runOptions.signalingUrl ? runOptions.signalingUrl.toString() : DEFAULT_SIGNALING_SERVER,
-    runOptions.iceUrls ? runOptions.iceUrls : DEFAULT_ICE_URLS
+    runOptions.iceUrls ? runOptions.iceUrls : DEFAULT_ICE_URLS,
+    runOptions.holochainRustLog,
+    runOptions.holochainWasmLog
   );
 
   // Install happ if necessary
