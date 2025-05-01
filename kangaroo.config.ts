6245f99ba15e29fc315cf8bc826e803739932258
Matthias <tuttiacc@hotmail.com>
2025-05-01 15:55:12 +0200

    bump version

--- kangaroo.config.ts
+++ kangaroo.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from './src/main/defineConfig';
 export default defineConfig({
   appId: 'org.holochain.kangaroo-electron',
   productName: 'Holochain Kangaroo Electron',
-  version: '0.3.3',
+  version: '0.3.4',
   macOSCodeSigning: false,
   windowsEVCodeSigning: false,
   fallbackToIndexHtml: true,
