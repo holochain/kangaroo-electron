03b77f459fee35053ba091494ed83e51331b6fa4
Matthias <tuttiacc@hotmail.com>
2025-05-21 23:02:01 +0200

    change to afterSign hook

---
+++ scripts/notarize.mjs
@@ -0,0 +1,30 @@
+import fs from 'fs';
+import path from 'path';
+import { notarize } from '@electron/notarize';
+
+module.exports = async function (params) {
+  if (process.platform !== 'darwin') {
+    return;
+  }
+
+  console.log('afterSign hook triggered', params);
+
+  const appId = 'holochain.launcher.electron';
+
+  const appPath = path.join(params.appOutDir, `${params.packager.appInfo.productFilename}.app`);
+  if (!fs.existsSync(appPath)) {
+    console.log('skip');
+    return;
+  }
+
+  console.log(`Notarizing ${appId} found at ${appPath}`);
+
+  await notarize({
+    appBundleId: appId,
+    appPath: appPath,
+    appleId: process.env.APPLE_ID_EMAIL,
+    appleIdPassword: process.env.APPLE_ID_PASSWORD,
+    teamId: process.env.APPLE_TEAM_ID,
+  });
+  console.log(`Done notarizing ${appId}`);
+};
