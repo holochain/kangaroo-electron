c2f706606074d182ca265c172f8ede00ff64c30e
Matthias <tuttiacc@hotmail.com>
2025-05-21 23:25:59 +0200

    undo changes

--- scripts/notarize.js
+++ scripts/notarize.js
@@ -1,6 +1,6 @@
-import fs from 'fs';
-import path from 'path';
-import { notarize } from '@electron/notarize';
+const fs = require('fs');
+const path = require('path');
+const { notarize } = require('@electron/notarize');
 
 module.exports = async function (params) {
   if (process.platform !== 'darwin') {
@@ -9,18 +9,23 @@ module.exports = async function (params) {
 
   console.log('afterSign hook triggered', params);
 
+  // const appId = 'holochain.launcher.electron';
+
   const appPath = path.join(params.appOutDir, `${params.packager.appInfo.productFilename}.app`);
   if (!fs.existsSync(appPath)) {
     console.log('skip');
     return;
   }
 
-  console.log(`Notarizing app found at ${appPath}`);
+  // console.log(`Notarizing ${appId} found at ${appPath}`);
 
   await notarize({
+    // appBundleId: appId,
     appPath: appPath,
     appleId: process.env.APPLE_ID_EMAIL,
     appleIdPassword: process.env.APPLE_ID_PASSWORD,
     teamId: process.env.APPLE_TEAM_ID,
   });
+  // console.log(`Done notarizing ${appId}`);
+    console.log(`Done notarizing`);
 };
