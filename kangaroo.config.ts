4ea58520c518c168254f603c6c2a0aeac809e1bd
Nicolas Luck <nicolas@lucksus.eu>
2026-01-12 14:30:46 +0100

    chore: bump binaries to 0.7.0-dev.6 with iroh

--- kangaroo.config.ts
+++ kangaroo.config.ts
@@ -14,30 +14,30 @@ export default defineConfig({
   signalUrl: 'wss://dev-test-bootstrap2.holochain.org/',
   iceUrls: ['stun:stun.cloudflare.com:3478', 'stun:stun.l.google.com:19302'],
   bins: {
-    holochainVersion: '0.6.0',
-    holochainFeature: "go-pion",
+    holochainVersion: '0.7.0-dev.6',
+    holochainFeature: 'iroh',
     holochain: {
       sha256: {
         'x86_64-unknown-linux-gnu':
-          '8ae1282b93bcba7eaee1343831895dd3b8628d4fd8ebc5fad74def3e073e6055',
+          '8cbbdf96f65d502aa827ff8c5f813a1599d4b7220c0a320f8a652fc4fdcbb086',
         'aarch64-unknown-linux-gnu':
-          '9e5e8d81625201f7146d42e628b563e6db4be1b866dc06e053266cb971e4c952',
+          '54cda827ef805b57b20e97d8f55ea64cae96583387a976a5bce3bc5f022a32fe',
         'x86_64-pc-windows-msvc.exe':
-          '9f838ab57b277e895b949cfec5c8a88c36d5936cce2e902706f92133d8029e77',
-        'x86_64-apple-darwin': 'd927ae095e5dbc683ae47cca336a220529f7b705d77d456cb7e4a3f2fec56adc',
-        'aarch64-apple-darwin': '0fcb1b7080496368438576b59a360a68bfa5c077ec8c83d6b5fd3d12b63b9e21',
+          'fc597e43dfbe8dfb907adaf23a3f6ee2204fc11718bb9cb15e055ced5d894de7',
+        'x86_64-apple-darwin': '252109620eba91bd29cd64610e53fbf789f5967becef4f087ca049f4b20e0a3d',
+        'aarch64-apple-darwin': '98396bae8f227c63e4b83d32dd9d0a017e2aac5f4082f1ad646b4a20255913be',
       },
     },
     lair: {
       sha256: {
         'x86_64-unknown-linux-gnu':
-          '19bde044278161948958c8b32d9bc0744550eccd6d29260da0045fc8c0480ccd',
+          'abd747b505a89ff19350c9f2f311e8c084e9efc5af4597469e36d2cc6560eca1',
         'aarch64-unknown-linux-gnu':
-          '2f4d084bb4904be8669bceb63516e5db201deee97294ba01c94ab62ba9ee8c02',
+          '253a3575787223dfda147fac26047c979137886283f277f73456d8985797ed1f',
         'x86_64-pc-windows-msvc.exe':
-          'b7b29197e028f807924c1f31cd6a332af63fd3c21d8fd563ba3c0a16f7197499',
-        'x86_64-apple-darwin': '030ae46320780b53db5a8047df534da4fd1e5d8900e7864b965b1243b2dfcb40',
-        'aarch64-apple-darwin': '4ae31f48ea92c7b6f472c2f57535f5c427918bf1876ab75b89673e0b242aaa1c',
+          '7f273fcbd35e78d84d4bc5d23f1edd1ee3339b415b5cd69d328ada8ed02814be',
+        'x86_64-apple-darwin': '14281be29b9a7eb3ba9f19aa66c07f9370ad39baf344d161122463d3a1c9b1cb',
+        'aarch64-apple-darwin': '072c24de6e2fa8ae8f8fbda1cb3de0475f061913e8e388cfdd17a2d378069c7e',
       },
     },
   },
