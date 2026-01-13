03550559b79c1985062ef94f92dbdc1b0aec4646
Jost <28270981+jost-s@users.noreply.github.com>
2026-01-13 10:21:46 -0600

    chore: sync fork with upstream (#7)
    * chore: Bump to 0.6.0 (#87)
    
    * feat: switch to using official holochain binaries (#89)
    
    ---------
    
    Co-authored-by: Christian Visintin <christian.visintin@veeso.dev>
    Co-authored-by: matthme <36768177+matthme@users.noreply.github.com>
    Co-authored-by: Jost Schulte <jost-s@users.noreply.github.com>

--- scripts/fetch-binaries.js
+++ scripts/fetch-binaries.js
@@ -14,7 +14,14 @@ fs.mkdirSync(binariesDir, { recursive: true });
 let targetEnding;
 switch (process.platform) {
   case 'linux':
-    targetEnding = 'x86_64-unknown-linux-gnu';
+    switch (process.arch) {
+      case 'arm64':
+        targetEnding = 'aarch64-unknown-linux-gnu';
+        break;
+      case 'x64':
+        targetEnding = 'x86_64-unknown-linux-gnu';
+        break;
+    }
     break;
   case 'win32':
     targetEnding = 'x86_64-pc-windows-msvc.exe';
@@ -38,16 +45,17 @@ switch (process.platform) {
 const binariesAppendix = kangarooConfig.appId.slice(0, 10).replace(' ', '-');
 
 const holochainBinaryFilename = `holochain-v${
-  kangarooConfig.bins.holochain.version
+  kangarooConfig.bins.holochainVersion
 }-${binariesAppendix}${process.platform === 'win32' ? '.exe' : ''}`;
 
-const lairBinaryFilename = `lair-keystore-v${kangarooConfig.bins.lair.version}-${binariesAppendix}${
+const lairBinaryFilename = `lair-keystore-${binariesAppendix}${
   process.platform === 'win32' ? '.exe' : ''
 }`;
 
 function downloadHolochainBinary() {
-  const holochainBinaryRemoteFilename = `holochain-v${kangarooConfig.bins.holochain.version}-${targetEnding}`;
-  const holochainBinaryUrl = `https://github.com/matthme/holochain-binaries/releases/download/holochain-binaries-${kangarooConfig.bins.holochain.version}/${holochainBinaryRemoteFilename}`;
+  const holochainBinaryRemoteFilename = `holochain-${kangarooConfig.bins.holochainFeature ? `${kangarooConfig.bins.holochainFeature}-` : ''}${targetEnding}`;
+  const holochainBinaryUrl = `https://github.com/holochain/holochain/releases/download/holochain-${kangarooConfig.bins.holochainVersion}/${holochainBinaryRemoteFilename}`;
+
   const destinationPath = path.join(binariesDir, holochainBinaryFilename);
   downloadFile(
     holochainBinaryUrl,
@@ -58,8 +66,7 @@ function downloadHolochainBinary() {
 }
 
 function downloadLairBinary() {
-  const lairBinaryRemoteFilename = `lair-keystore-v${kangarooConfig.bins.lair.version}-${targetEnding}`;
-  const lairBinaryUrl = `https://github.com/matthme/holochain-binaries/releases/download/lair-binaries-${kangarooConfig.bins.lair.version}/${lairBinaryRemoteFilename}`;
+  const lairBinaryUrl = `https://github.com/holochain/holochain/releases/download/holochain-${kangarooConfig.bins.holochainVersion}/lair-keystore-${targetEnding}`;
   const destinationPath = path.join(binariesDir, lairBinaryFilename);
   downloadFile(lairBinaryUrl, destinationPath, kangarooConfig.bins.lair.sha256[targetEnding], true);
 }
