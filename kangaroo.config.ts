4d483e07b6e7c7cc90657aeb2cf0673491ab00ac
Jost <jost-s@users.noreply.github.com>
2026-07-20 10:41:07 +0200

    feat: upgrade to holochain 0.7.0-rc.1

--- kangaroo.config.ts
+++ kangaroo.config.ts
@@ -15,29 +15,29 @@ export default defineConfig({
   signalUrl: 'wss://dev-test-bootstrap2.holochain.org/',
   iceUrls: ['stun:stun.cloudflare.com:3478', 'stun:stun.l.google.com:19302'],
   bins: {
-    holochainVersion: '0.7.0-dev.31',
+    holochainVersion: '0.7.0-rc.1',
     holochain: {
       sha256: {
         'x86_64-unknown-linux-gnu':
-          'efe975ec1003756ca73071c7a54c43356d7c201e3b7723a1cbefd3d9775637ad',
+          'c4538d7b0e7f15053096967c4f779c84d214aaff814b62a77852cd0fbde108e5',
         'aarch64-unknown-linux-gnu':
-          '62bf75aaaeb1e97d3f07c6b0f5b45032f71b8d325b4e8fe6f5ca53bbda2633e8',
+          'f541f7b46332affabb1fdd53f9c9797586418782a021d293fb69f06fc0dacee3',
         'x86_64-pc-windows-msvc.exe':
-          'adf3d998b27641a3ec34637a2d9c3897d9d91ee3714edc74f4dde48037f97473',
-        'x86_64-apple-darwin': '202e362fa0e127ce90c9b843977c465fa4e9c8a2bcbbe14a0a30fd08f89f6c9c',
-        'aarch64-apple-darwin': '99a70e08cdcab33cbb67ad53c9e879bfe5258dab3d722241d9fcbce469158204',
+          '82b87ecd87247db793b2971a5fc8a601def0a1d4827c74af9be893b978b794dc',
+        'x86_64-apple-darwin': 'fe82874dad77922d64427994f99e1eeb7ae47956bb7a61dc3c1a14795a0a2cd1',
+        'aarch64-apple-darwin': 'd5454db7e2c3283cf753fc3d60f29e81103e1ccbc46ccdd5c4c734cfaeb3f935',
       },
     },
     lair: {
       sha256: {
         'x86_64-unknown-linux-gnu':
-          '65c428ae60e4cb210113c29df5991fdda51a0bf8cb88de0166709abe5eb90a37',
+          'ac03e332adf4c08526ac24866931d9ce419b6512bcae76a5e38c36e7eeba1340',
         'aarch64-unknown-linux-gnu':
-          'b176d23880916e56033ff4ff8bb27b49b973ffc62eed323fc906d5a0005cef27',
+          '110581a12c5d46429fdc20f6999e1563d6dc1d32406afa2dc7c0c5d400798fa5',
         'x86_64-pc-windows-msvc.exe':
-          'dc7965ef315ac050add2cf24aeeb6fc33808814fa2fea3620bd40c3c87fee9ad',
-        'x86_64-apple-darwin': 'acc534058f8daaefbcbe0418a45c0f6ca4fbae78aad47909f0987193c463417b',
-        'aarch64-apple-darwin': '316704901ec7362bfae6c5e8207ae3f417b2f69b73bc3845ff89196a738ad2c4',
+          'b09b859cdda1566a3524ab1ab467f6e94a16d1bc304d30546a705c3dca5a90b6',
+        'x86_64-apple-darwin': '5de8ccf27d95a7bdf91860df09dd480c91826ec0b8e6ba2c47dc237cee221979',
+        'aarch64-apple-darwin': 'ad44c70b0e13f8b33632e3e441bff225507c4ff0720bbfa465d9bf7e2e9c9e20',
       },
     },
   },
