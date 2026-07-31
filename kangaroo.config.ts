9584b62f0b2ae9c29f2aed2bfc0f6b8903129c48
ThetaSinner <ThetaSinner@users.noreply.github.com>
2026-07-31 12:25:44 +0100

    feat: Upgrade to Holochain 0.7.0

--- kangaroo.config.ts
+++ kangaroo.config.ts
@@ -13,29 +13,29 @@ export default defineConfig({
   bootstrapUrl: 'https://dev-test-bootstrap2.holochain.org/',
   relayUrl: 'https://dev-test-bootstrap2.holochain.org/',
   bins: {
-    holochainVersion: '0.7.0-rc.5',
+    holochainVersion: '0.7.0',
     holochain: {
       sha256: {
         'x86_64-unknown-linux-gnu':
-          'd04c0bb63b704c2977960ec4ee247246c1d1a795fafbdeb669fed557863c2b99',
+          'ffa40a0c6fab5ce062c4af76328dfe2de143256ddf791a504d72bca698a9ba20',
         'aarch64-unknown-linux-gnu':
-          'a3f43992a5705fdcb1e15ee757b99f28177a09c51348fa246c683a29361c66a6',
+          'a01a6b0be4a6fa52276fb0aca94559a05457bbad495bdd01802208d3f3649e51',
         'x86_64-pc-windows-msvc.exe':
-          '45ed158070ae52a107811c28cf12623831ff8b12b65fd7076fd6626e1df61c8f',
-        'x86_64-apple-darwin': '1f4a13840090720f0d626799cc297b9b1ff0c347f4d39f5233cb13de04fecbe1',
-        'aarch64-apple-darwin': '6ff03513fe759aa2a62b9041e0b36f2e0e9425b1d7d606fd86b58aae0ec1624b',
+          '5d9bd8cf28c45645158d56198cfec38d04ac4cf620ba89ca37db2f7c46d7b3d2',
+        'x86_64-apple-darwin': '53d48d0f92fdfb2d70cd302a3782df142ab324c023943319d2c33226a79dea0c',
+        'aarch64-apple-darwin': 'fa44e56402294221cad34933bfe12e079460aa22acf98723f5b4e68e3e33cc3b',
       },
     },
     lair: {
       sha256: {
         'x86_64-unknown-linux-gnu':
-          '94dd9de3d9a77b0806f2f8f586f726f96cace319568fc8ec2930b8d3e84abac2',
+          '7a77822ab5e0020d0f3c358030d4ccfa8c6c144407a5c075d302c7b0fcf670c1',
         'aarch64-unknown-linux-gnu':
-          '5ca630cafa3a9c5e85fcd3b2a7bde5a22f89c67515e8d9014ea13c13771b690f',
+          'efb5894d76ee8d58845445e1b6a3ff18d721345943e545457331825fb799b08c',
         'x86_64-pc-windows-msvc.exe':
-          'ddb5cf1bb9569e0aafdb04fbc7717622f6e2b7867fb110207e9f2c16b0cfa152',
-        'x86_64-apple-darwin': '4df6b5e038cf01cdab32e643e1e5d439191712fb90d062ac21a0a719e9b7937b',
-        'aarch64-apple-darwin': 'e6098b3344d59002e8c9675e442569fdd7705b67613b68bb3b7f5e10cdfb16c9',
+          '2c1fded6eb7c6ec4b3d033c6a310d7c2cc916d6e0ac10c2f71047ebb6b623f6b',
+        'x86_64-apple-darwin': 'afcaefac4f93828e59566f185d7398b7d5d7afaa43ef4b95bd9c42f0b967d7b8',
+        'aarch64-apple-darwin': 'e8fcdf26c0b1a51e5ab93a82333b5db92f0a8a6effb8494fdbb0dc000856ded8',
       },
     },
   },
